6f7d292dbf89a193e0c11e99f90213cc6da0d434
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-29 10:26:21 +0530

    Create 01_Variables_data-types.py

---
+++ CHAPTER 2/01_Variables_data-types.py
@@ -0,0 +1,12 @@
+a=10  # A is an Interger 
+
+b=10.55 # B is an floating point number
+
+c="Abhay" #C is string 
+
+d=True  #d is a boolean variable
+
+e=None  #c is a none type variable 
+
+
+print(a+b)
