9b19e37a20bca6718ca173912bd9951187b0c6fa
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-29 10:28:11 +0530

    Create Input.py

---
+++ CHAPTER 2/Input.py
@@ -0,0 +1,8 @@
+# input is used to take an input from the user...
+a = int(input("Enter the number of a : "))
+b = int(input("Enter the number of b : "))
+
+print("The number in a is : ",a)
+print("The number in b is : ",b)
+
+print("The sum of the above number is : ",a+b)
