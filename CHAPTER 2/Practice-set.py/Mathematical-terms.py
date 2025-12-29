3a7ef3097f85cfb70a67f14c55ba982a080d09bf
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-29 10:36:13 +0530

    Create Mathematical-terms.py

---
+++ CHAPTER 2/Practice-set.py/Mathematical-terms.py
@@ -0,0 +1,26 @@
+# Write a program to calculate the square of given number 
+
+a = int(input("Enter a number : "))
+
+square = a*a
+
+print("The square of given numebr is : ",square)
+
+
+# Write a program to find the average of 2 numbers 
+
+b = int(input("Enter the first number : "))
+c = int(input("Enter the second numebr : "))
+
+d = (b+c)/2
+
+print("The averag eof these two numbers is : ",d)
+
+
+# Write a program to find which number is greater using comparison
+
+e = 34
+f = 80
+
+g = e>f   # This will print false...
+print(g)
