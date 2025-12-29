1bec01d82b16a0f6e7e8658f347c3f1d655a8428
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-29 10:27:23 +0530

    Create Type-casting.py

---
+++ CHAPTER 2/Type-casting.py
@@ -0,0 +1,14 @@
+# Type is used to convert  the one data type into another data type
+# Also used to show the data type of the variable 
+
+a=10
+b=type(a)
+# print(b)  Output : <class 'int'>
+
+# To convert
+c=float(a)
+# print(c)   Output : 10.0
+
+d = 10
+e = str(d)
+print(e) 
