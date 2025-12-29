d7dbad2125e0e93abf33563739efef1be95a1c13
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-29 08:50:30 +0530

    Create Module.py

---
+++ CHAPTER 1/Module.py
@@ -0,0 +1,15 @@
+# This file is about importing the modules and how to use them.....
+
+import pyjokes
+
+# Single line comment.
+
+Chutkule = pyjokes.get_joke()
+
+"""
+Multi 
+Line 
+Comments
+"""
+
+print(Chutkule)
