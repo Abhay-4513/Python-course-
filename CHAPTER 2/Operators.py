2b3fcc3d6364a0074c55c41d8b6feb8c9da2cc23
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-29 10:26:50 +0530

    Create Operators.py

---
+++ CHAPTER 2/Operators.py
@@ -0,0 +1,37 @@
+# Arithmetic operators
+a=10
+b=5
+c=(a+b)
+print(c)
+
+# Assignment operators
+d=10
+d +=5  # Increment d by 5 
+print(d)
+
+e=5
+e -=2  # Decrement e by 2
+print(e) 
+
+f=5
+f *=2  # Multiply 5 by 2
+print(f)
+
+# Comparison operators
+g = 5>4
+print(g) 
+h = 10<2
+print(h)
+i = 5==5
+print(i)
+j = 5!=10
+print(j)
+
+
+# Logical oprators 
+k = True or False
+print(k)
+L = True and False
+print(L) 
+M = not True
+print(M)
