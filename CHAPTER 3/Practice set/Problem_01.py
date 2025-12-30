7391118b93dc31d8649d5bce1a66a0f530dce8fa
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-30 10:18:26 +0530

    Delete CHAPTER 3/Practice set directory

--- CHAPTER 3/Practice set/Problem_01.py
+++
@@ -1,7 +0,0 @@
-# Write a python program to display a user name followed by Good Afternoon using input() function 
-Name = input("Enter your name : ")
-
-print("Good Afternoon",Name)
-print(f"Good Afternoon {Name} ")
-
-# Problem solved 
