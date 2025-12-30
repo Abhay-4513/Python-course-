1f9b8d463734c7675b7ebd3d147683af697762dd
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-30 10:17:57 +0530

    Create problem_01.py

---
+++ CHAPTER 3/Practice-set/problem_01.py
@@ -0,0 +1,11 @@
+# Write a python program to display a user name followed by Good Afternoon using input() function 
+Name = input("Enter your name : ")
+
+print("Good Afternoon",Name)
+print(f"Good Afternoon {Name} ")
+
+# Write a program to format letter sentence using escape sequence
+
+letter = "Dear Abhay \n\tThis python course is nice.\nThanks !"
+
+print(letter)
