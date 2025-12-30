c520a0465bec386fcd73459f4b867b604db09886
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-30 10:14:26 +0530

    Create String-functions.py

---
+++ CHAPTER 3/String-functions.py
@@ -0,0 +1,23 @@
+name = "abhay"
+
+b=len(name)
+
+c = name.endswith("hay")
+d = name.startswith("bh")
+e= name.count("a")
+f = name.capitalize()
+g = name.find("h")
+h = name.replace("abhay","Hello")
+
+
+print(b)
+print(c) 
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+# ESCAPE SEQUENCE
+Sentence = "I\t am a \"student\" at \'JSPM\' university\n in 2nd year"
+print(Sentence)
