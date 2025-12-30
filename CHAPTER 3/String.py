0828df1604f4c75eed5998a0b174ad69e35f4e41
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-30 10:11:25 +0530

    Create String.py

---
+++ CHAPTER 3/String.py
@@ -0,0 +1,11 @@
+a = "Abhay"
+
+ashort=a[1:4]   # This is used to print specific word from specific index
+                # The last index will be excluded
+print(ashort)
+
+LENGTH = len(a)   # len print the length of the word
+print(LENGTH)
+
+SIngle = a[1]   # This print only single character 
+print(SIngle)
