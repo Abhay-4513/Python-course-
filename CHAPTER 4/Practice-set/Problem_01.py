c9be2d7f486ecfe4680cb906876e7370f22beff0
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-30 10:25:41 +0530

    Create Problem_01.py

---
+++ CHAPTER 4/Practice-set/Problem_01.py
@@ -0,0 +1,45 @@
+#Accept 7 fruits name from user and store them in a list 
+
+Fruits = []
+
+f1 = input("Enter fruit 1 name: ")
+Fruits.append(f1)
+f2 = input("Enter fruit 2 name: ")
+Fruits.append(f2)
+f3 = input("Enter fruit 3 name: ")
+Fruits.append(f3)
+f4 = input("Enter fruit 4 name: ")
+Fruits.append(f4)
+f5 = input("Enter fruit 5 name: ")
+Fruits.append(f5)
+f6 = input("Enter fruit 6 name: ")
+Fruits.append(f6)
+f7 = input("Enter fruit 7 name: ")
+Fruits.append(f7)
+print(Fruits)
+
+#Problem solved 
+
+
+# Enter 6 students marks and sort them
+
+Marks = []
+
+s1 = int(input("Enter student 1 marks: "))
+Marks.append(s1)
+s2 = int(input("Enter student 2 marks: "))
+Marks.append(s2)
+s3 = int(input("Enter student 3 marks: "))
+Marks.append(s3)
+s4 = int(input("Enter student 4 marks: "))
+Marks.append(s4)
+s5 = int(input("Enter student 5 marks: "))
+Marks.append(s5)
+s6 = int(input("Enter student 6 marks: "))
+Marks.append(s6)
+
+
+Marks.sort()
+print("The sorted list os student marks :",Marks)
+
+# Problem solved
