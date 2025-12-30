fc5dec4ff70fbb71b49e8c6c9f5ae5237f197f50
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-30 10:26:28 +0530

    Create Problem_02.py

---
+++ CHAPTER 4/Practice-set/Problem_02.py
@@ -0,0 +1,14 @@
+# Write a program to sum a list with 4 numbers
+
+Number = [34,54,94,100]
+
+print(sum(Number))
+
+# problem solved
+
+# Write a program to count the number of zeros in the following tuple
+
+a = [7,0,8,0,0,9]
+print(a.count(0))
+
+# Problem solved 
