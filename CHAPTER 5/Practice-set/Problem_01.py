af995f05c5509761b25968133c155ee2b187622e
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-31 13:58:42 +0530

    Create Problem_01.py

---
+++ CHAPTER 5/Practice-set/Problem_01.py
@@ -0,0 +1,15 @@
+# Write a program to create a dictionary of hindi books with values as their english translation.Provide user with a option to look it up 
+
+Meaning = {
+    "Rang":"Colour",
+    "Ghar":"Home",
+    "Kapade":"Clothes",
+    "Mitti":"Soil",
+    "Sangnak":"Computer"
+}
+
+Word = input("Enter a word you want meaning of: ")
+
+print(Meaning[Word])
+
+# Problem solved 
