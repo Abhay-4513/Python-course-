b33ef289b67bc5d4e98d21a3184fc139ad6fec81
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-31 14:05:34 +0530

    Create Problem_05.py

---
+++ CHAPTER 6/Practice-set/Problem_05.py
@@ -0,0 +1,12 @@
+# Write a program which finds out whether a given name is present in a list or not.
+
+
+print("Enter correct spelling and starting letter should be capital .")
+Fruits = ["Mango","Banana","Apple","Graphes"]
+
+a = input("Enter fruit name : ")
+
+if(a in Fruits):
+    print("Fruit name founded ! ")
+else:
+    print("Try again.....")
