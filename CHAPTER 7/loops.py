59604be1df03099b0bdc7de11fab993146361b39
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-01-02 08:57:33 +0530

    Create loops.py

---
+++ CHAPTER 7/loops.py
@@ -0,0 +1,18 @@
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+print()
+print()
+# Now using loops :
+
+for i in range(1,100):
+    print(i)
+
+# While loop
+a = 1
+while(a<51):
+    print(a)
+    a = a + 1  # or we can use i = i + 1
