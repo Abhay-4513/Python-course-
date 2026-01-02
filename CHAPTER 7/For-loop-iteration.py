d7eddc90f74ad55765ec88eecbd800b899ac665a
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-01-02 08:58:23 +0530

    Create For-loop-iteration.py

---
+++ CHAPTER 7/For-loop-iteration.py
@@ -0,0 +1,12 @@
+l = [4,8,7,9,5,22,66]  # Iterating the list 
+for i in l:
+    print(i)
+
+t = (55,45,85,99,105)  # Iterating the tupple 
+for i in t:     
+    print(i)
+
+s = "Abhay"   # Iterating the string 
+for i in s:
+    print(i)
+
