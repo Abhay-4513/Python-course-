02a1424bac8815551fa1e9446319bae206e035b2
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-01-03 08:38:07 +0530

    Create Break-continue.py

---
+++ CHAPTER 7/Break-continue.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if(i == 34):
+        break # Exit the loop right now
+    print(i)
+
+for i in range(100):
+    if(i == 0):
+        continue   # This will skip the value of i , here 0 is skipped
+    print(i)
