271cd530f64f72a6ecadd0260004b4e0dc600c15
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-01-28 22:11:47 +0530

    Add files via upload

---
+++ CHAPTER 8/02_Default_argument.py
@@ -0,0 +1,8 @@
+def goodDay(name, ending="Thank you"):
+
+    print(f"Good day, {name}")
+    print(ending)
+
+goodDay("Abhay")  # Here by default the ending value will be Thank you
+ 
+goodDay("Abhishek", "Thanks")  # Here no default value will be applied because we have given the value.............
