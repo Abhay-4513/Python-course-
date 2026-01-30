54eaaf56f36da124827bbaffac4bfb6112fd51c2
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-01-30 23:19:13 +0530

    Create problem_02.py

---
+++ CHAPTER 8/Practice-set/problem_02.py
@@ -0,0 +1,6 @@
+# Write a program to convert temperature from celsius to fahrenheit using the functions....
+def temConvert(f):
+    return 5*(f-32)/9
+
+f = int(input("Enter the temperature : "))
+print(temConvert(f))
